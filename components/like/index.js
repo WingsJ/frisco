395e621e5b777d4b7971d3e07436c9cb140f680f
WingsJ <1599788511@qq.com>
2019-01-18 16:00:59 +0800

    组件的事件,属性

--- components/like/index.js
+++ components/like/index.js
@@ -1,25 +1,44 @@
 // components/like/index.js
 Component({
   /**
-   * 组件的属性列表
+   * 组件的属性列表 开放
    */
   properties: {
+    like: {
+      type: Boolean,//数据类型 必填
+      value: false, //初始值 选填
+      observer: () => {
 
+      }
+    },
+    count: {
+      type: Number,
+      value: 1,
+      observer: () => {
+
+      }
+    }
   },
 
   /**
-   * 组件的初始数据
+   * 组件的初始数据 封装
    */
   data: {
-    count:8,
+    trueImg: 'images/like.png',
+    falseImg: 'images/like@dis.png',
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onLike:(event) =>{
-      console.log(111)
+    onLike(event) {
+      let like = this.properties.like;
+      let count = this.properties.count;
+      this.setData({
+        like: !like,
+        count: like ? --count : ++count
+      })
     }
   }
 })

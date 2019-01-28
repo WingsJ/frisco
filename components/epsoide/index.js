481787a54547a22d3f27a30f008e389bff82d5aa
18767151671 <wangjie45>
2019-01-29 00:09:14 +0800

    “01/29“01/29 epsoide组件完成

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -20,13 +20,20 @@ Component({
    * 组件的初始数据
    */
   data: {
+    months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
     year: 0,
     month: '',
     _index:'',
   },
   //组件加载时
   attached() {
-
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    this.setData({
+      year,
+      month: this.data.months[month],
+    })
   },
   /**
    * 组件的方法列表

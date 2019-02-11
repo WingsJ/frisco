5b95914f6524f785736b64f64c3d844fd6da3401
18767151671 <wangjie45>
2019-02-12 00:20:48 +0800

    数据缓存运用

--- modules/classic.js
+++ modules/classic.js
@@ -1,40 +1,56 @@
-import {HTTP} from '../utils/http.js'
-class ClassicModel extends HTTP{
+import { HTTP } from '../utils/http.js'
+class ClassicModel extends HTTP {
   //最新期
-  getLatest(sCallBack){
+  getLatest(sCallBack) {
     this.request({
       url: 'classic/latest',
       success: (data) => {
         this._setLatestIndex(data.index)
+        wx.setStorageSync(this._getKey(data.index), data)
         sCallBack(data)
       }
     })
   }
   //上一期or下一期
-  getClassic(sCallBack, index, nextOrprevious){
-    this.request({
-      url: `classic/${index}/${nextOrprevious}`,
-      success: (data) => {
-        sCallBack(data)
-      }
-    })
+  getClassic(sCallBack, index, nextOrprevious) {
+    //缓存orAPI写入
+    //key 确定key
+    let key = nextOrprevious === 'next' ? this._getKey(index + 1) : this._getKey(index - 1);
+    let classic = wx.getStorageSync(key)
+    if (!classic) {
+      this.request({
+        url: `classic/${index}/${nextOrprevious}`,
+        success: (data) => {
+          wx.setStorageSync(this._getKey(data.index), data)
+          sCallBack(data)
+        }
+      })
+    } else {
+      sCallBack(classic)
+    }
   }
   //判断第一期
-  isFirst(index){
+  isFirst(index) {
     let latestIndex = this._getLatestIndex();
     return index !== latestIndex ? true : false
   }
   //判断最后一期
-  isLatest(index){
+  isLatest(index) {
     return index !== 1 ? true : false
   }
-  _setLatestIndex(index){
+
+  _getKey(index) {
+    let key = `classic-${index}`;
+    return key
+  }
+
+  _setLatestIndex(index) {
     wx.setStorageSync('latestIndex', index)
   }
-  _getLatestIndex(){
-    let index = wx.getStorageSync('latestIndex'); 
+  _getLatestIndex() {
+    let index = wx.getStorageSync('latestIndex');
     return index
   }
 }
 
-export { ClassicModel}
+export { ClassicModel }
